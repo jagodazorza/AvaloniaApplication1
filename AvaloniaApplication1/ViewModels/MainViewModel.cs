7070f6c4b984c97b6750c5042ff9cf6bf76bb54d
jagodazorza <kotkotowski080@gmail.com>
2025-04-17 11:14:08 +0200

    Dodaj pliki projektów.

---
+++ AvaloniaApplication1/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Interactivity;
+using System.Xml;
+
+namespace AvaloniaApplication1.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+
+   
+}
---
+++ AvaloniaApplication1/Views/MainView.axaml.cs
@@ -0,0 +1,63 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Metadata;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvaloniaApplication1.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    public void DNA(string s)
+    {
+        if (s is null) return;
+        
+        List<string> list = new List<string>();
+       
+
+        for (int i = 0; i <= s.Length - 4; i++)
+        {
+            list.Add(s.Substring(i, 4));
+        }
+
+
+        List<int> licznik = new List<int>(new int[list.Count]);
+
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            licznik[i]++;
+            for (int j = 0; j < list.Count; j++)
+            {
+                if (list[i] == list[j] && i!=j)
+                {
+                    licznik[i]++;
+                }
+            }
+        }
+
+      
+
+
+        StringBuilder wynikBuilder = new StringBuilder();
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            wynikBuilder.Append(i+1 + ". " + list[i] + ": " + licznik[i] + "\n");
+        }
+        string wynik = wynikBuilder.ToString();
+
+        DNA_Output.Text = wynik;
+    }
+
+    public void ButtonClicked(object? sender, RoutedEventArgs e)
+    {
+        string sekwencja = TekstBox.Text;
+        DNA(sekwencja);
+    }
+}
