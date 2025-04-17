4b5040bdc5ee2f38822a40b61f2dca8f23d1dc48
jagodazorza <kotkotowski080@gmail.com>
2025-04-17 19:29:18 +0200

    Działa dobrze i poprawnie

--- AvaloniaApplication1/Views/MainView.axaml.cs
+++ AvaloniaApplication1/Views/MainView.axaml.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Metadata;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -27,22 +28,37 @@ public partial class MainView : UserControl
 
 
         List<int> licznik = new List<int>(new int[list.Count]);
-
+        List<bool> toRemove = new List<bool>(new bool[list.Count]);
+            
+            
 
         for (int i = 0; i < list.Count; i++)
         {
             licznik[i]++;
+            if (toRemove[i] != true) toRemove[i] = false;
+
             for (int j = 0; j < list.Count; j++)
             {
                 if (list[i] == list[j] && i!=j)
                 {
                     licznik[i]++;
+                    if (i<j) 
+                    {
+                        toRemove[j] = true;
+                    }
                 }
             }
         }
 
-      
-
+        for (int i = toRemove.Count-1; i >= 0; i--)
+        {
+            if (toRemove[i])
+            {
+                licznik.Remove(licznik[i]);
+                list.Remove(list[i]);
+            }
+        }
+       
 
         StringBuilder wynikBuilder = new StringBuilder();
 
